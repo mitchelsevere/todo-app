621acbffcdd6d6f93ce5ffe1a3cc2fe10c809958
Mitchel Severe <truth531@gmail.com>
2017-07-29 15:42:52 -0400

    added method_override middleware

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(session({
   secret: process.env.SECRET_KEY,
