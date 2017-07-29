f96c0be88473aa0852ab75a1abd5d3054dd8c267
Mitchel Severe <truth531@gmail.com>
2017-07-29 16:22:10 -0400

    Added todo model

---
+++ models/todo.js
@@ -0,0 +1,44 @@
+const db = require('../db/config');
+
+const Todo = {};
+
+Todo.all = () => {
+  return db.query('SELECT * FROM todos');
+}
+
+Todo.byId = (id) => {
+  return db.one(`
+  SELECT * FROM todos
+  WHERE id = $/id/
+  `, [id]);
+}
+
+Todo.create = (todo, userid) => {
+  return db.one(`
+  INSERT INTO todos
+  (title, category, description, user_id)
+  VALUES 
+  ($/title/, $/category/, $/description/, $/user_id/)
+  RETURNING * 
+  `, [todo.title, todo.category, todo.description, userid]);
+}
+
+Todo.update = (todo, id) => {
+  return db.one(`
+  UPDATE todos SET
+  title = $/title/,
+  category = $/category/,
+  description = $/description/,
+  WHERE id = $/id/
+  RETURNING *
+  `, [todo.title, todo.category, todo.description, id]);
+}
+
+Todo.delete = (id) => {
+  return db.none(`
+  DELETE FROM todos
+  WHERE id = $/id/
+  `, [id]);
+}
+
+module.exports = Todo;
