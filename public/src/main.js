dd3e09902bad3186b674c7efaa5e4e4fe674ec96
Mitchel Severe <truth531@gmail.com>
2017-07-30 19:45:00 -0400

    Added button javascript

---
+++ public/src/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  // landing page button
+  $('#enter').click(function() {
+    window.location.href = '../todos/';
+  });
+  // add new todo button
+  $('#add-todo').click(function() {
+    window.location.href = '../todos/add';
+  })
+});
