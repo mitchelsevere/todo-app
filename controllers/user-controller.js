54f89d0a828f8459dad467a7925e47e15afe26fd
Mitchel Severe <truth531@gmail.com>
2017-07-30 13:29:53 -0400

    Added user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+// require bcrypt middleware
+const bcrypt = require('bcryptjs');
+// require User model
+const User = require('../models/user');
+// create empty controller object
+const userController = {};
+// create new user on register page
+userController.create = (req, res) => {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  User.add({
+    username: req.body.username,
+    email: req.body.email,
+    password_digest: hash,
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+  }).then(user => {
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/user');
+    });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({error: err});
+  });
+}
+
+module.exports = userController;
