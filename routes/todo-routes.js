27fa72ff77232130d12d7f0ead3ce3d4d2c882e1
Mitchel Severe <truth531@gmail.com>
2017-07-30 14:58:27 -0400

    Added login requirements for todos

--- routes/todo-routes.js
+++ routes/todo-routes.js
@@ -1,15 +1,15 @@
 // require express and initialise instance of express Router
 const express = require('express');
 const todoRouter = express.Router();
-// const authHelpers = require('../services/auth/auth-helpers');
+const authHelpers = require('../services/auth/auth-helpers');
 // require the controller for todos
 const todoController = require('../controllers/todo-controller');
 // get controller.index route (get all todos on home page)
 todoRouter.get('/', todoController.index);
 // post controller.create route (post a new todo (login required))
-todoRouter.post('/', todoController.create);
+todoRouter.post('/', authHelpers.loginRequired, todoController.create);
 // render add page (options to add a new todo (login required))
-todoRouter.get('/add', (req, res) => {
+todoRouter.get('/add', authHelpers.loginRequired, (req, res) => {
   res.render('todos/add-todo', {
     currentPage: 'Add Todo',
   });
@@ -17,10 +17,10 @@ todoRouter.get('/add', (req, res) => {
 // get controller.show route (get specific todo)
 todoRouter.get('/:id', todoController.show);
 // get controller.edit route (edit specific todo (login required))
-todoRouter.get('/:id/edit', todoController.edit);
+todoRouter.get('/:id/edit', authHelpers.loginRequired, todoController.edit);
 // get controller.update route (update specific todo (login required))
-todoRouter.put('/:id', todoController.update);
+todoRouter.put('/:id', authHelpers.loginRequired, todoController.update);
 // get controller.delete route (delete specific todo (login required))
-todoRouter.delete('/:id', todoController.delete);
+todoRouter.delete('/:id', authHelpers.loginRequired, todoController.delete);
 
 module.exports = todoRouter;
